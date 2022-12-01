37147fcb05c2c6faadeb6fe3e42cdee7ef9b1e65
leostelon <53221136+leostelon@users.noreply.github.com>
2022-12-01 10:26:31 +0530

    search api

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
-TESTS:
+----- APIS -----
 
-1. Null Metadata url
+--- Search API ---
+1. /api/nfts
+    - Retrieves the nfts
+    Query Params
+   limit=10
+   skip=10
+   chain_id=8080 (Don't confuse it with Polygon chain ID, can be changed later)
+   query=mynft
+   owner=0x320CC5B64B609703cB3Da5c7744E0991FD6C0675
+   createdAt=asc
--- src/routes/nft.js
+++ src/routes/nft.js
@@ -2,21 +2,87 @@ const { NFT } = require("../models/nft");
 
 const router = require("express").Router();
 
-router.get("/nft", async (req, res) => {
+router.get("/nfts", async (req, res) => {
 	try {
-		const nfts = await NFT.aggregate([
+		let query = req.query.query || "";
+		let owner = req.query.owner;
+		let chain_id = req.query.chain_id;
+		let queryOptions = {
+			$or: [
+				{ name: { $regex: query, $options: "i" } },
+				{ token_id: { $regex: query, $options: "i" } },
+				{ contract_address: { $regex: query, $options: "i" } },
+				{ metadata_url: { $regex: query, $options: "i" } },
+				{ description: { $regex: query, $options: "i" } },
+			],
+		};
+		if (chain_id) {
+			queryOptions.chain_id = chain_id;
+		}
+		let contract = req.query.contract;
+		if (contract) {
+			contract = req.query.contract.split(",").map((e) => new RegExp(e, "i"));
+			queryOptions.contract_address = { $in: contract };
+		}
+		let ownerQuery = {};
+		if (owner) {
+			ownerQuery["owners.address"] = new RegExp(owner, "i");
+		}
+
+		const assets = await NFT.aggregate([
 			{
-				$facet: {
-					paginatedResults: [{ $skip: 0 }, { $limit: 1 }],
-					totalCount: [
+				$match: {
+					...queryOptions,
+				},
+			},
+			{
+				$sort: { createdAt: req.query.createdAt === "asc" ? 1 : -1, _id: 1 },
+			},
+			{
+				$lookup: {
+					from: "events",
+					as: "events",
+					let: { asset_id: "$_id" },
+					pipeline: [
 						{
-							$count: "count",
+							$match: {
+								$expr: { $eq: ["$asset_id", "$$asset_id"] },
+							},
 						},
+						{ $sort: { createdAt: -1 } },
+						{ $limit: 2 },
 					],
 				},
 			},
+			{
+				$lookup: {
+					from: "owners",
+					as: "owners",
+					let: { id: "$_id" },
+					pipeline: [
+						{
+							$match: {
+								$expr: { $eq: ["$nft_id", "$$id"] },
+							},
+						},
+						{ $limit: 10 },
+					],
+				},
+			},
+			// This match slows down the query
+			{
+				$match: {
+					...ownerQuery,
+				},
+			},
+			{
+				$unwind: { path: "$owner", preserveNullAndEmptyArrays: true },
+			},
+			{ $skip: parseInt(!req.query.skip ? 0 : req.query.skip) },
+			{ $limit: parseInt(!req.query.limit ? 10 : req.query.limit) },
 		]);
-		res.send(nfts);
+
+		res.send(assets);
 	} catch (error) {
 		res.status(500).send({ message: error.message });
 	}
